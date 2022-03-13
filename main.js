83863281147af486841004084bb9ce8354ed2e0a
UploaderVincent <96601799+UploaderVincent@users.noreply.github.com>
2022-03-12 16:54:33 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(580,100);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+function draw() {
+    background('#808080');
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initailized!');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
